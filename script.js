da64b38a3141b2dabd67b3bc076f5359b093de2c
Srushti-K-01 <ksrushtimahesh@gmail.com>
2025-02-06 11:49:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function firstWord(s) {
   // your code here
+	if (s === "" || s.indexOf(" ") === -1) {
+    return s;
+  } else {
+    return s.substring(0, s.indexOf(" "));
+  }
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
