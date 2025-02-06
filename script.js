69fe09d2789078d06b8ae55ce679967a2acf5488
Srushti-K-01 <ksrushtimahesh@gmail.com>
2025-02-06 12:49:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function firstWord(s) {
   // your code here
+// 	s = s.trim();
+//     let spaceIndex = s.indexOf(" ");
+//     if (spaceIndex === -1) {
+//         return s;
+//     }
+//     return s.substring(0, spaceIndex);
+// }
 	s = s.trim();
-    let spaceIndex = s.indexOf(" ");
-    if (spaceIndex === -1) {
-        return s;
-    }
-    return s.substring(0, spaceIndex);
-}
+	let words = s.split(" ");
+	return words[0] || s;
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
